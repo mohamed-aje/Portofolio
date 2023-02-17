9350bd9179e361a02505e0cdfb067c8cf1ae1d46
mohamed-aje <chochoaj93@gmail.com>
2023-02-17 17:21:19 +0200

    fixed styles and added proper text

--- src/components/About.js
+++ src/components/About.js
@@ -7,26 +7,28 @@ export default function About() {
         <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium dark:text-white">
             Hi, I'm Mohamed.
-            <br className="hidden lg:inline-block" /> Lorem ipsum dolor sit
-            amet, consectetur adipisicing elit. Qui
+            <br className="hidden lg:inline-block" /> <br /> I love Building
+            Applications
           </h1>
           <p className="mb-8 leading-relaxed dark:text-white">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui
-            laborum quasi, incidunt dolore iste nostrum cupiditate voluptas?
-            Laborum, voluptas natus?
+            I’m a highly motivated fresh graduate, I'm capable of handling
+            multiple tasks on daily basis, dependable person who is great at
+            time management, I’m honest and trustworthy and my personal saying
+            is “vision without action is just a daydream” because I believe that
+            with hard work and dedication, you can make any dream a reality.
           </p>
           <div className="flex justify-center">
             <a
               href="#contact"
-              className="inline-flex dark:text-white dark:bg-green-500 border-0 py-2 px-6 focus:outline-none dark:bg-green-600 rounded dark:text-lg"
+              className="inline-flex text-white bg-green-500 border-0 py-2 px-6 focus:outline-none hover:bg-green-600 rounded "
             >
-              Work With Me
+              Contact me
             </a>
             <a
               href="#projects"
-              className="ml-4 inline-flex dark:text-gray-400 dar:bg-gray-800 border-0 py-2 px-6 focus:outline-none dark:bg-gray-700 dark:text-white rounded dark:text-lg"
+              className="ml-4 inline-flex text-gray-400 bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-gray-700 hover:text-white rounded "
             >
-              See My Past Work
+              explore my projects
             </a>
           </div>
         </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,25 +9,24 @@ import { skills } from "../data";
 
 const Skills = () => {
   return (
-    <section id="skills" className="dark:bg-gray-800">
+    <section id="skills" className="dark:bg-gray-900 ç md:sticky top-0 z-10">
       <div className="container px-5 py-10 mx-auto">
         <div className="text-center mb-20">
           <CpuChipIcon className="w-10 inline-block mb-4" />
-          <h1 className="sm:text-4xl text-3xl font-medium title-font dark:text-white mb-4">
+          <h1 className="sm:text-4xl text-3xl font-medium title-font text-white mb-4">
             Skills &amp; Technologies
           </h1>
           <p className="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi sit
-            ipsa delectus eum quo voluptas aspernatur accusantium distinctio
-            possimus est.
+            I gained my skills and experiences through workin with school
+            projects and hobby projects and work experience.
           </p>
         </div>
-        <div className="flex flex-wrap  lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div className="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
           {skills.map((skill) => (
             <div key={skill} className="p-2 sm:w-1/2 w-full">
-              <div className=" rounded flex p-4 h-full items-center">
+              <div className="bg-gray-800 rounded flex p-4 h-full items-center">
                 <RocketLaunchIcon className="text-green-400 w-6 h-6 flex-shrink-0 mr-4" />
-                <span className="title-font font-medium dark:text-white">
+                <span className="title-font font-medium text-white">
                   {skill}
                 </span>
               </div>
