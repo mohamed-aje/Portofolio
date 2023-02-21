4bab6ef837fc9a7b1b3e642860c743ee924fec99
mohamed-aje <chochoaj93@gmail.com>
2023-02-21 14:39:11 +0200

    changed font family to serif

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,12 @@
 module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      body: ['"Open Sans"'],
+    },
+  },
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
 };
